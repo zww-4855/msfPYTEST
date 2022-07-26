b4782d98b7951521cb47f7fd6559a996ac124e04
Zachary Windom <z.windom@ufl.edu>
2022-07-26 11:41:10 -0400

    Add test for the calculate angle function

--- msf_2022b/tests/test_msf_2022b.py
+++ msf_2022b/tests/test_msf_2022b.py
@@ -8,7 +8,25 @@ import sys
 import pytest
 
 import msf_2022b
+import numpy as np
 
+def test_calculate_angle():
+    """ Test that calculate_angle function correctly determines the angle"""
+    r1=np.array([0,0,-1])
+    r2=np.array([0,0,0])
+    r3=np.array([1,0,0])
+    expected_angle=np.pi/2.0
+    output_angle=msf_2022b.calculate_angle(r1,r2,r3)
+    assert expected_angle==output_angle
+
+def test_calculate_distance():
+    """ Test that the calculate_distance function calculates what we expect"""
+    r1=np.array([0,0,0])
+    r2=np.array([0,1.0,0])
+
+    expected_output=1.0
+    observed_output=msf_2022b.calculate_distance(r1,r2)
+    assert expected_output == observed_output
 
 def test_msf_2022b_imported():
     """Sample test, will always pass so long as import statement worked."""
