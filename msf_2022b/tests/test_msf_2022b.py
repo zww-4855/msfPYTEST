489f57aacdc53f8ce3f4b9e268de1abf662c7d02
Zachary Windom <z.windom@ufl.edu>
2022-07-26 14:13:40 -0400

    Add function tests

--- msf_2022b/tests/test_msf_2022b.py
+++ msf_2022b/tests/test_msf_2022b.py
@@ -10,6 +10,40 @@ import pytest
 import msf_2022b
 import numpy as np
 
+@pytest.fixture
+def methane_molecule():
+    symbols=np.array(["C","H","H","H","H"])
+    coordinates=np.array([
+        [1,1,1],
+        [2.4,1,1],
+        [-0.4,1,1],
+        [1,1,2.4],
+        [1,1,-0.4],
+    ])
+
+
+@pytest.mark.parametrize("p1,p2,p3,expected_angle",[
+    (np.array([0,0,-1]), np.array([0,0,0]), np.array([1,0,0]),90),
+    (np.array([np.sqrt(2)/2,np.sqrt(2)/2,0]),np.array([0,0,0]),np.array([1,0,0]),45)
+    ]
+)
+def test_calculate_angle_many(p1,p2,p3,expected_angle):
+    calculated_angle = msf_2022b.calculate_angle(p1,p2,p3,degrees=True)
+    assert expected_angle == calculated_angle, calculated_angle
+
+
+
+def test_build_bond_list_error():
+    coordinates=np.array([
+        [0,0,0],
+        [0,1.0,0],
+        [1,1,1]
+    ])
+
+    with pytest.raises(ValueError):
+        msf_2022b.build_bond_list(coordinates,min_bond=-1)
+
+@pytest.mark.skip
 def test_calculate_angle():
     """ Test that calculate_angle function correctly determines the angle"""
     r1=np.array([0,0,-1])
